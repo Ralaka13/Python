9523602988c45b55f7396d316bb5d167a523cefb
Greg Warner <warnerg13@gmail.com>
2021-04-17 01:21:35 -0500

    First Testing Exercises complete

---
+++ Learning/script.py
@@ -0,0 +1,78 @@
+#print("Hello, World")
+
+#myint = 7
+#print(myint)
+
+#myfloat = 7.0
+#print(myfloat)
+
+#yfloat = float(7)
+#print(myfloat)
+
+#mystring = 'hello'
+#print(mystring)
+#mystring = "hello"
+#print(mystring)
+
+#mystring = "Don't worry about using apostrophes"
+#print(mystring)
+
+#one = 1
+#two = 2
+#three = one + two
+#print(three)
+
+#hello = "hello"
+#world = "world"
+#helloworld = hello + " " + world
+#print(helloworld)
+
+#a, b = 3, 4
+#print(a,b)
+
+#This will not work
+#one = 1
+#two = 2
+#hello = "hello"
+
+#print(one + two + hello)
+
+# change this code
+#mystring = "hello"
+#myfloat = float(10)
+#myint = 20
+
+# testing code
+#if mystring == "hello":
+#    print("String: %s" % mystring)
+#if isinstance(myfloat, float) and myfloat == 10.0:
+#    print("Float: %f" % myfloat)
+#if isinstance(myint, int) and myint == 20:
+#    print("Integer: %d" % myint)
+
+#mylist = []
+#mylist.append(1)
+#mylist.append(2)
+#mylist.append(3)
+#print(mylist[0])
+#print(mylist[1])
+#print(mylist[2])
+
+#for x in mylist:
+#    print(x)
+
+#mylist = [1,2,3]
+#print(mylist[10])
+
+#Exercise Lists
+numbers = [1,2,3]
+strings = ["hello","world"]
+names = ["John","Eric","Jessica"]
+
+#write code here
+second_name = names[1]
+
+#this code should write out the filled arrays and the second name in the names list (Eric)
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
