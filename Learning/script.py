bda1331544929f05bf7cddabdc7658c83690d071
Greg Warner <warnerg13@gmail.com>
2021-04-17 23:02:28 -0500

    Completeled another exercise

--- Learning/script.py
+++ Learning/script.py
@@ -121,3 +121,39 @@
 #    print("Almost there...")
 #if big_list.count(x) == 10 and big_list.count(y)  == 10:
 #    print("Great!")
+
+#String Formatting
+#Python uses C-style string formatting to create new, formatted strings. The "%" operator is used to format a set of variables enclosed in a "tuple" (a fixed size list), together with a format string, which contains normal text together with "argument specifiers", special symbols like "%s" and "%d".
+
+#Let's say you have a variable called "name" with your user name in it, and you would then like to print(out a greeting to that user.)
+
+name = "John"
+print("Hello, %s!" % name) 
+
+
+name = "John"
+age = 23
+print("%s is %d years old." %(name, age))
+
+mylist = [1,2,3]
+print("A lsit: %s" % mylist)
+
+#Here are some basic argument specifiers you should know:
+
+#%s - String (or any object with a string representation, like numbers)
+
+#%d - Integers
+
+#%f - Floating point numbers
+
+#%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+#%x/%X - Integers in hex representation (lowercase/uppercase)
+
+#Exercise
+#You will need to write a format string which prints out the data using the following syntax: Hello John Doe. Your current balance is $53.44.
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current account balance is %s"
+
+print(format_string % data)
